3bbe24e6a86f13c3df2f653605559eda804e1ad6
Taha Ince <taha.ince@developia.io>
2024-09-20 00:55:37 +0300

    Feat: DataAnnotations

--- Micromarin.Service/Entities/DynamicEntity.cs
+++ Micromarin.Service/Entities/DynamicEntity.cs
@@ -5,7 +5,9 @@ namespace Micromarin.Services.Entities;
 public class DynamicEntity {
     [Key]
     public int Id { get; set; }
+    [MaxLength(100, ErrorMessage = "The maximum length for the name is 100 characters.")]
     public required string Type { get; set; }
+    [MaxLength(1000, ErrorMessage = "The maximum length for the name is 1000 characters.")]
     public required string Data { get; set; } = "{}";
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
